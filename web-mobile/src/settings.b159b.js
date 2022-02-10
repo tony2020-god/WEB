832b4c03097f31c65d0b38645103020fdaa4e97e
tony2020-god <62175480+tony2020-god@users.noreply.github.com>
2022-02-10 09:45:43 +0800

    套紅包

--- web-mobile/src/settings.b159b.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/ropepack/RoleConfirm_ropepack.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",main:"3de49"}};
