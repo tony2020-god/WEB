5cd1b523e7522ae3ada05b76b5510b45c3e21757
tony2020-god <62175480+tony2020-god@users.noreply.github.com>
2022-02-22 10:00:05 +0800

    搖紅包

--- web-mobile/src/settings.db4e7.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/1_redpack/RoleConfirm_redpack.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",main:"4ac59"}};
