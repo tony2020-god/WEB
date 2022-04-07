cc9fce801c4cb8e618dd2cc4bfa20272da580d27
tony2020-god <62175480+tony2020-god@users.noreply.github.com>
2022-04-07 11:35:41 +0800

    搖紅包

--- web-mobile/src/settings.af48f.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/3_whack_a_mole/RoleConfirm_whack_mole.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",resources:"11d3b",main:"84440"}};
