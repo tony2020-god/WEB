de9c8fb9ddbddee3ac7876b6b03a82ab1fa86601
tony2020-god <62175480+tony2020-god@users.noreply.github.com>
2022-04-22 14:54:34 +0800

    遊戲抽獎

--- web-mobile/src/settings.e702b.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/5_lottery/RoleConfirm_lottery.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",resources:"11d3b",main:"55baa"}};
